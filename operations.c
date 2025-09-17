65e88afb88ebc88c0d5818d40ec0b140997dc9d7
singharnavcj-source <singharnav.cj@gmail.com>
2025-09-17 17:15:50 +0530

    Add files via upload

---
+++ operations.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main () {
+    int a, b, c, d,e,f; 
+    printf("Enter two numbers"); 
+    scanf("%d" "%d", &a, &b); 
+    c=a+b;
+    d=a-b;
+    e=a*b;
+    f=a/b;
+    printf("Sum = %d", c); 
+    printf("Sum = %d", d);
+    printf("Sum = %d", e);
+    printf("Sum = %d", f);
+
+    return 0;
+}
