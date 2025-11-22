44c7b293a3c920b617f4b2e42f50aaab562748cf
singharnavcj-source <singharnav.cj@gmail.com>
2025-11-22 21:04:45 +0530

    Add files via upload

---
+++ Q40.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char binary[100];
+
+    printf("Enter a binary number: ");
+    scanf("%s", binary);
+
+    for (int i = 0; binary[i] != '\0'; i++) {
+        if (binary[i] == '0')
+            binary[i] = '1';
+        else if (binary[i] == '1')
+            binary[i] = '0';
+        else {
+            printf("Invalid binary digit encountered!\n");
+            return 1;
+        }
+    }
+
+    printf("1's Complement: %s\n", binary);
+
+    return 0;
+}
